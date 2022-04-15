6ef0f4ac47058b5016e61b46ca5da7e7d7952fb7
Wei Lun <garyganweilun@gmail.com>
2022-04-15 21:07:45 +0800

    Set up basic login routes

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const passport = require('passport')
+const session = require('express-session')
+const User = require('./models/user')
+const app = express();
+app.use(express.json())
+app.use(cors({ origin: process.env.FRONTEND_URL, methods: ['POST', 'PUT', 'GET', 'OPTIONS', 'HEAD', 'DELETE', 'PATCH'], credentials: true }));
+const userController = require('./controllers/login')
+app.enable('trust proxy')
+
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+    proxy : true ,
+    cookie: {sameSite: "none", secure: true}
+}));
+
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// use static serialize and deserialize of model for passport session support
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+app.use(userController)
+
+
+
+module.exports = app
---
+++ controllers/login.js
@@ -0,0 +1,77 @@
+const passport = require('passport')
+require("dotenv").config();
+const express = require('express')
+const User = require('../models/user')
+const router = express.Router()
+// router.use(express.static("public"))
+const LocalStrategy = require('passport-local').Strategy
+
+
+passport.use(new LocalStrategy(User.authenticate())); // use static authenticate method of model in LocalStrategy
+
+router.post('/signup', async (req, res, next) => {
+    console.log(req.body.username)
+    const findUser = await User.findOne({ username: req.body.username })//when user click signup, this will find the user by the username that they input
+    if (!findUser) {//if no user found in DB, register the user.
+        await User.register(new User({ username: req.body.username }), req.body.password, (err) => {
+            if (err) { //if any error while registering, send the error message(cases like exisitng user name)
+                res.status(400)
+                res.send(err)
+                return next()
+            }
+            else {
+                res.send({ message: "User signup Successful" })
+            }
+        })
+
+    }
+    else {
+        res.send({ message: "User signup Error" })
+    }
+})
+
+router.post('/login', async (req, res, next) => { // this is to login the user
+    await passport.authenticate('local', (err, user, info) => { // check if the user details and password are in our DB
+        if (err) { // if error, then send an error
+            res.status(400)
+            return res.send({ message: err })
+        }
+        if (!user) { // if user not found, we tell user that the credentials that they entered is incorrect.
+            res.status(400)
+            return res.send({ message: "Password or username is incorrect" })
+        }
+        req.logIn(user, (err) => { // else, means user found, we log the user in and tell user that he login successfully
+            if (err) { return next(err) }
+            return res.send({ message: "User login Successful" })
+        })
+    })(req, res, next)
+
+});
+
+router.get('/getlogin', (req, res) => { // this is to check the user session.
+    res.send(req.user)
+})
+
+
+router.delete('/logout', (req, res) => { //this will log the user out. Clear the cookies to remove any session
+
+    if (req.session) {
+        req.logOut()
+        req.session.destroy((err) => {
+            if (err) {
+                res.send(err)
+            } else {
+                res.clearCookie('connect.sid')
+                res.send({ message: "You are successfully logged out!" })
+            }
+        })
+    }
+    else {
+        res.send({ message: "You are not logged in!" })
+    }
+
+
+
+})
+
+module.exports = router
